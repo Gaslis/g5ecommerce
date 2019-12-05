3e57ca8b50236725f36fd75144edea4f10e46384
Gas <gaslis@gmail.com>
2019-12-05 16:53:22 -0300

    Update listadoXcategoria.blade.php

--- resources/views/products/listadoXcategoria.blade.php
+++ resources/views/products/listadoXcategoria.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.plantilla')
 @section('content')
     <h2 class="text-center py-4">{{$categoriaXcat->name}}</h2>
-
-
     <div class="spacer px-5">
       <table class="table">
         <thead>
@@ -10,7 +8,7 @@
             <th>Id</th>
             <th>Nombre</th>
             <th>Ver</th>
-            <th>Editar</th>
+            {{-- <th>Editar</th> --}}
             {{-- <th>Eliminar</th> --}}
         </tr>
         </thead>
@@ -21,7 +19,7 @@
                 <td>{{$producto->id}}</td>
                 <td>{{$producto->nombre}}</td>
                 <td><a href="/products/detalleProducto/{{$producto->id}}"><ion-icon name="eye"></ion-icon></a></td>
-                <td><a href="/products/editarProducto/{{$producto->id}}"><ion-icon name="create"></ion-icon></a></td>
+                {{-- <td><a href="/products/editarProducto/{{$producto->id}}"><ion-icon name="create"></ion-icon></a></td> --}}
                 {{-- <td><a href="/products/eliminarProducto/{{$value->id}}"><ion-icon name="trash"></ion-icon></td></a>
                 </tr> --}}
 
