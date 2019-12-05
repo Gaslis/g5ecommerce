75b8d35149c3536c46fb27eb8f84039fba8c873f
Gas <gaslis@gmail.com>
2019-12-05 16:29:56 -0300

    Create formularioContacto.blade.php

---
+++ resources/views/products/formularioContacto.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.plantilla')
+@section('content')
+  <section class="limitesFormu">
+    <h1 class="h1-faq">FORMULARIO DE CONTACTO</h1>
+    <form>
+      <div class="form-row">
+        <div class="col">
+          <input type="text" class="form-control" placeholder="First name">
+        </div>
+        <div class="col">
+          <input type="text" class="form-control" placeholder="Last name">
+        </div>
+      </div>
+    <div class="form-row">
+      <div class="col">
+        <input type="email" class="form-control" placeholder="Email">
+      </div>
+      <div class="col">
+      <input type="number" class="form-control" placeholder="Teléfono">
+      </div>
+    </div>
+    <input class="form-control form-control-lg" type="text" placeholder="Mensaje">
+    <button type="submit" class="btn btn-primary-login botonFormulario">ENVIAR</button>
+    </form>
+  </section>
+@endsection
