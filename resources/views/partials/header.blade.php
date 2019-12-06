ccacaeb43b261fca4e055d42550134ce8c407ab4
Gas <gaslis@gmail.com>
2019-12-06 12:02:28 -0300

    Update header.blade.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -29,7 +29,7 @@
     <a class="dropdown-item" href="#">Link 2</a>
     <a class="dropdown-item" href="#">Link 3</a>
   </div>
-  <a href="carrito.php">
+  <a href="/carrito/index">
     <button type="submit" class="btn btn-primary botonChango"><ion-icon name="cart"></ion-icon></button>
   </a>
 </div>
