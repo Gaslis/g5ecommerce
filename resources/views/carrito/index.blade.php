573e5f2c6732666ede999eda362234f0e5849709
Gas <gaslis@gmail.com>
2019-12-06 14:57:36 -0300

    Update index.blade.php

--- resources/views/carrito/index.blade.php
+++ resources/views/carrito/index.blade.php
@@ -16,11 +16,11 @@
 
           @foreach ($carrito as $value)
               <tr>
-              <td>{{$carrito->id}}</td>
-              <td>{{$carrito->user_id}}</td>
-              <td>{{$carrito->product_id}}</td>
-              <td>{{$carrito->precio_unitario}}</td>
-              <td>{{$carrito->cantidad}}</td>
+              <td>{{$value->id}}</td>
+              <td>{{$value->user_id}}</td>
+              <td>{{$value->product_id}}</td>
+              <td>{{$value->precio_unitario}}</td>
+              <td>{{$value->cantidad}}</td>
               </tr>
 
           @endforeach
