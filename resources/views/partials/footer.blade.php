a58f07d26b9bd70d2bddff34b803dc6f84c3e6ac
Gas <gaslis@gmail.com>
2019-12-06 09:03:57 -0300

    Update footer.blade.php

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,8 +3,8 @@
     <article class="col-12 col-sm-4 col-md-4 mb-5">
       <h4>Ayuda</h4>
       <a href="faq.php">Preguntas Frecuentes</a><br>
-      <a href="/products/formularioContacto/">Como hacer tu pedido</a><br>
-      <a href="/products/formularioContacto/">Quienes Somos</a><br>
+      <a href="/products/formularioContacto">Como hacer tu pedido</a><br>
+      <a href="/products/formularioContacto">Quienes Somos</a><br>
     </article>
     <article class="col-12 col-sm-4 col-md-4 mb-5">
       <h4>Contacto</h4>
